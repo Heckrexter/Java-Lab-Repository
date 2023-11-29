e0b50e1f847d27d85846b807069020bb85251d98
Heckrexter <102521099+Heckrexter@users.noreply.github.com>
2023-11-29 15:21:26 +0530

    idk

--- prac/four.java
+++
@@ -1,8 +0,0 @@
-public class four {
-    public static void main(String[] args) {
-        System.out.println("Multiplication table of 5");
-        for (int i = 1; i <= 10; i++) {
-            System.out.println("5 x "+i+" = "+5*i);
-        }
-    }
-}
